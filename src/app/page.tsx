2d70cae8a6094a1a1c2479d8b9a0fe361e62ddf6
Fernanda <febaccarini@gmail.com>
2024-12-26 21:43:04 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <main className="w-full flex flex-col items-center justify-center">
+      <h1>Dashboard de Qualidade</h1>
+    </main>
+    </>
+  );
+}
